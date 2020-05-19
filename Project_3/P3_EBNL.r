ce386e885dbb1baffbf158cbe2fba23eb5eab624
Nora Lüpkes <nora.luepkes@rwth-aachen.de>
2020-05-19 12:54:28 +0200

    after discussion with ET changed mod_1 and added mod_2

--- Project_3/P3_EBNL.r
+++ Project_3/P3_EBNL.r
@@ -15,4 +15,17 @@ library(quantreg)
 
 summary(fhm.data)
 
-mod_1 <- glm.nb(new_cases ~ new_cases + day_nbr, offset(log(population)), data = fhm.data)
+fhm.data$region <- factor(fhm.data$region) 
+fhm.data$region <- relevel(fhm.data$region, "Stockholm")
+
+mod_1 <- glm.nb(new_cases ~ day_nbr_region + day_nbr, offset(log(population)), data = fhm.data)
+summary(mod_1)
+step(mod_1, k=log(nrow(fhm.data)))
+
+ mod_1.coeff <- mod_1$coefficients
+
+# plot confidence intervall and prediction intervall
+
+mod_2 <- glm.nb(new_cases ~ (day_nbr_region)*(day_nbr)*obs_date + region, offset(log(population)), data = fhm.data)
+summary(mod_2)
+step(mod_2, k=log(nrow(fhm.data)))
