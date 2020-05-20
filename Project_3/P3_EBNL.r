75dc03c37c6f0e113f0eaf1c40b42d222e2a80a3
Nora Lüpkes <nora.luepkes@rwth-aachen.de>
2020-05-20 11:02:21 +0200

    1a and 1b, with questions

--- Project_3/P3_EBNL.r
+++ Project_3/P3_EBNL.r
@@ -15,6 +15,20 @@ library(quantreg)
 
 summary(fhm.data)
 
+(
+  plot_data <- ggplot(data = fhm.data, 
+                      aes(x = obs_date, y = new_cases)) + 
+    geom_point(size = 1) +
+    xlab("observation data") +
+    ylab("new cases")+
+    theme(text = element_text(size = 16))
+)
+(
+  plot_data + 
+    geom_smooth(method = lm, se = FALSE) +
+    labs(caption = "fhm data and fitted line")
+)
+
 fhm.data$region <- factor(fhm.data$region) 
 fhm.data$region <- relevel(fhm.data$region, "Stockholm")
 
@@ -22,7 +36,18 @@ mod_1 <- glm.nb(new_cases ~ day_nbr_region + day_nbr, offset(log(population)), d
 summary(mod_1)
 step(mod_1, k=log(nrow(fhm.data)))
 
- mod_1.coeff <- mod_1$coefficients
+mod_1.coeff <- mod_1$coefficients
+confint(mod_1)
+
+# basic residual analysis
+fhm_pred <- 
+  cbind(fhm.data, 
+        pred = predict(mod_1, interval = "prediction"))
+
+fhm_pred$e <- mod_1$residuals
+# Save the max-value in order to make the y-axis symmetrical in the plots.
+(max.e <- max(abs(fhm_pred$e)))
+(fhm_elims <- c(-max.e, max.e))
 
 # plot confidence intervall and prediction intervall
 
